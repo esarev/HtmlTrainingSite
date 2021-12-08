661d86f61e8e2f4c34307afaf5679fdf543fee1c
Alex <esarev@inbox.ru>
2021-12-09 02:01:33 +0700

    first commit

---
+++ components/about.php
@@ -0,0 +1,7 @@
+<section class="about container">
+  <h2 class="section-title">Привет, я <?= $name ?></h2>
+  <img class="about-image" src="<?= $image ?>" width="147" height="147" alt="<?= $name ?>">
+  <div class="about-content">
+    <p>Имею базовые знания таких программ, как: Sony Vegas Pro, Adobe Photoshop, Webflow, CMS WordPress, Figma. Проходил курсы в онлайн школе Максима Соколова по специальности: верстальщик сайта на Webflow, а также на онлайн платформе "Html-academy" курсы по веб-разработке.  Эта сфера мне очень интересна и я готов погрузиться в неё полностью.</p>
+  </div>
+</section>
---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+$title = 'Сайт обучайт!';
+$name = 'Alex';
+$image = 'img/avatar.jpg';
+$email = 'esarev@inbox.ru';
+$phone = '+79994694305';
+
+require('components/header.php');
+require('components/about.php');
+require('components/status.php');
+require('components/photos.php');
+require('components/news.php');
+require('components/quotes.php');
+require('components/contacts.php');
+require('components/footer.php');
